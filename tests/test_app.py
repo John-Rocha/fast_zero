7c168e069e622562e9f6f79ab845f34c5affc856
Johnathan Rocha <johnathanrocha@gmail.com>
2025-01-08 22:17:32 -0300

    feat: add tests for user creation and retrieval; refactor root test to use fixture

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,51 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
 
+
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client):
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test',
+            'email': 'test@test.com',
+            'password': '123456',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test@test.com',
+    }
+
+
+def test_read_users(client):
+    client = TestClient(app)
+
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {'id': 1, 'username': 'test', 'email': 'test@test.com'}
+        ]
+    }
