87fe0f9baa5125926c0bc5dca1250e7b0c0f9cb9
Johnathan Rocha <johnathanrocha@gmail.com>
2025-01-09 21:00:27 -0300

    feat: CRUD completed

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
@@ -44,3 +44,38 @@ def create_user(user: UserSchema):
     user_list.append(user_with_id)
 
     return user_with_id
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    user_index = next(
+        (index for index, user in enumerate(user_list) if user.id == user_id),
+        None,
+    )
+
+    if user_index is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    user_list[user_index] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    user_index = next(
+        (index for index, user in enumerate(user_list) if user.id == user_id),
+        None,
+    )
+
+    if user_index is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del user_list[user_index]
+
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -19,8 +19,6 @@ def test_root_deve_retornar_ok_e_ola_mundo(client):
 
 
 def test_create_user(client):
-    client = TestClient(app)
-
     response = client.post(
         '/users/',
         json={
@@ -39,13 +37,68 @@ def test_create_user(client):
 
 
 def test_read_users(client):
-    client = TestClient(app)
-
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'users': [
-            {'id': 1, 'username': 'test', 'email': 'test@test.com'}
-        ]
+        'users': [{'id': 1, 'username': 'test', 'email': 'test@test.com'}]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'john',
+            'email': 'john@test.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'john',
+        'email': 'john@test.com',
+    }
+
+
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/999',
+        json={
+            'username': 'john',
+            'email': 'john@test.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+    response = client.put(
+        '/users/0',
+        json={
+            'username': 'john',
+            'email': 'john@test.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/999')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+    response = client.delete('/users/0')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
